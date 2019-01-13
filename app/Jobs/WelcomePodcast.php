bf20c311d502d956e4bfb9c00894f1d32ac4b78f
Chayan Datta <sonicxxx7@gmail.com>
2019-01-14 01:20:49 +0530

    Mail class added

--- app/Jobs/WelcomePodcast.php
+++ app/Jobs/WelcomePodcast.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Mail\WelcomeMailer;
+use Illuminate\Support\Facades\Mail;
 
 class WelcomePodcast implements ShouldQueue
 {
